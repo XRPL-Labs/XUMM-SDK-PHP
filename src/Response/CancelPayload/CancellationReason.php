37cbe010cb077bd5e27997ff1208503f0a5d67ef
Wietse Wind <w.wind@ipublications.net>
2022-03-11 14:18:13 +0100

    Add Payload Cancel OK enum response

--- src/Response/CancelPayload/CancellationReason.php
+++ src/Response/CancelPayload/CancellationReason.php
@@ -4,6 +4,7 @@ namespace Xrpl\XummSdkPhp\Response\CancelPayload;
 
 enum CancellationReason:string
 {
+    case OK = 'OK';
     case ALREADY_CANCELLED = 'ALREADY_CANCELLED';
     case ALREADY_RESOLVED = 'ALREADY_RESOLVED';
     case ALREADY_OPENED = 'ALREADY_OPENED';
