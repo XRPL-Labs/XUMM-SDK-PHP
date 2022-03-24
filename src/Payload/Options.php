73c6c4a845ef33d4fc70cc0a8270bdeb4fe63fb0
Pauline Vos <pvos88@gmail.com>
2022-03-24 13:09:16 +0100

    Change `expire` typehint to integer
    As according to XUMM API docs.

--- src/Payload/Options.php
+++ src/Payload/Options.php
@@ -7,7 +7,7 @@ final class Options
     public function __construct(
         public readonly ?bool $submit = null,
         public readonly ?bool $multisign = null,
-        public readonly ?bool $expire = null,
+        public readonly ?int $expire = null,
         public readonly ?bool $immutable = null,
         public readonly ?bool $forceAccount = null,
         public readonly ?ReturnUrl $returnUrl = null,
