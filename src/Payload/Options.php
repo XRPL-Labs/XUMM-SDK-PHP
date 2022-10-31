918cdab34d420696fb8a23b525407883b0b2eeb1
Julian Gums <julian@synt.ax>
2022-10-31 17:31:59 +0000

    Update Options.php

--- src/Payload/Options.php
+++ src/Payload/Options.php
@@ -1,17 +1,23 @@
 <?php
-
+declare (strict_types=1);
 namespace Xrpl\XummSdkPhp\Payload;
 
 final class Options
 {
+    /**
+     * @param array<string>|null $signers
+     */
     public function __construct(
         public readonly ?bool $submit = null,
         public readonly ?bool $multisign = null,
         public readonly ?int $expire = null,
         public readonly ?bool $immutable = null,
         public readonly ?bool $forceAccount = null,
-        public readonly ?ReturnUrl $returnUrl = null,
         public readonly ?array $signers = null,
+        public readonly ?ReturnUrl $returnUrl = null,
     ) {
+        if($signers) $this->typeArrayNullString(...$signers);
+    }
+    private function typeArrayNullString(?string ...$arg): void {
     }
 }
