8cf260a4ceb9e0ec9829e0b44a3c74bfdd4c51b4
Dennis Mwea <mweadennis2@gmail.com>
2022-11-03 10:10:55 +0300

    Change exception class being thrown

---
+++ src/Exception/Payload/InvalidArgumentException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Xrpl\XummSdkPhp\Exception\Payload;
+
+use Exception;
+use Xrpl\XummSdkPhp\Exception\XummException;
+
+class InvalidArgumentException extends Exception implements XummException
+{
+}
