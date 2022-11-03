f86e74bbbbe999763edf6375d0910ce2079cd815
Dennis Mwea <mweadennis2@gmail.com>
2022-11-03 10:26:07 +0300

    Remove multiple signers param

--- src/Payload/Options.php
+++ src/Payload/Options.php
@@ -20,7 +20,6 @@ final class Options
         public readonly ?bool $forceAccount = null,
         public readonly ?array $signers = null,
         public readonly ?ReturnUrl $returnUrl = null,
-        public readonly ?array $signers = null,
     ) {
         foreach ($this->signers as $key => $signer) {
             $this->validate($signer, $key);
