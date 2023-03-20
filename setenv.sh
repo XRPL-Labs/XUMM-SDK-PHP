15889f737b25870c2a0e08d3e9b11887e09340c7
Pauline <pv@ddln-pv.zrh.daedalean.ai>
2023-03-20 23:29:23 +0100

    Fix setting environment variables
    They need to be sourced from a bash script if they're gonna work
    reliably as it turns out

---
+++ setenv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export MODE="test"
+export XUMM_API_KEY="aaaaaaaa-bbbb-cccc-dddd-1234567890ab"
+export XUMM_API_SECRET="cbbbbbbb-aaaa-cccc-dddd-1234567890ab"
+export XUMM_TEST_BASE_URI="http://localhost:8080"
